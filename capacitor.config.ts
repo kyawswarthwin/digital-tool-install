2e8edbe34711b731c2af505c72170301be7c8563
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2023-06-20 08:50:37 +0630

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.kyawswar.digitaltoolinstall',
+  appName: 'Digital Tool Install',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
